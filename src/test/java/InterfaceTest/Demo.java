dfbfefb5a748de6a19398d2efd8c2a07a11e75a2
Administrator <805194823@qq.com>
2017-04-24 15:30:18 +0800

    alal

--- src/test/java/InterfaceTest/Demo.java
+++ src/test/java/InterfaceTest/Demo.java
@@ -8,6 +8,6 @@ import UIFrameWork.BrowserUtils;
 public class Demo {
     public static void main(String[] args) {
 
-        System.out.println("sb");
+        System.out.println("sbca");
     }
 }
