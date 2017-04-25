971b42e6b901888463964697369172a4ff48f308
Administrator <805194823@qq.com>
2017-04-25 20:02:14 +0800

    alaaq

---
+++ src/test/java/InterfaceTest/TestLogin.java
@@ -0,0 +1,25 @@
+package InterfaceTest;
+
+import Interface.login;
+import InterfaceFrameWork.RequestUtils;
+import Utils.ExcelIterator;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created by YZQ on 2017-04-24.
+ */
+public class TestLogin {
+    @DataProvider(name = "login")
+    public Iterator<Object[]>getdata() throws IOException {
+        return new ExcelIterator("tinyshop\\tinyshop_test","获取token_login");
+    }
+    @Test(dataProvider = "login")
+    public  void login(Map<String,String>map){
+        String response= login.Login(map.get("请求路径"));
+    }
+}
