16311dc4ed2c0d55c7c3358e2aa3267664ab9518
tang <18711624029@163.com>
2017-04-25 08:33:53 +0800

    新建测试

--- src/test/java/InterfaceTest/Demo.java
+++ src/test/java/InterfaceTest/Demo.java
@@ -1,13 +1,10 @@
 package InterfaceTest;
 
-import UIFrameWork.BrowserUtils;
-
 /**
  * Created by Administrator on 2017-04-24.
  */
 public class Demo {
     public static void main(String[] args) {
-
         System.out.println("sbcahf");
     }
 }
